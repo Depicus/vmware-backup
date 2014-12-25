e648ca1dc3f571d0887e2cb89d7a603872d066a4
depicus <Depicus@users.noreply.github.com>
2014-12-25 21:13:55 +0000

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,7 @@
+# Author: Brian Slack - Depicus
+# Created Date: 25/Dec/2014
+# http://www.depicus.com/
+##################################################################
+
+# run the backup
+./ghettoVCB.sh -f machines -g ./machines.conf
